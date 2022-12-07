db1efbfab12182d5a4cb217539dec723f14183e5
Элис <73361849+ElizavetaBerdina@users.noreply.github.com>
2022-12-07 18:39:27 +0300

    Add files via upload

---
+++ hw1_after/swagger_api/flask_restapi/flask_restapi.py
@@ -0,0 +1,192 @@
+from flask import jsonify, json
+import flask
+from flask_restx import reqparse, Resource, Api, fields
+from swagger_api.main_models.main_models import models_storege, available_models
+from swagger_api.main_models.main_models import fit_model, get_preds, get_params, delete_model
+import numpy as np
+import pandas as pd
+
+
+app = flask.Flask(__name__)
+api = Api(app)
+available_models_classes = list(available_models.keys())
+
+
+@api.route('/api/model_classes')
+class model_classes(Resource):
+    @staticmethod
+    def get():
+        try:
+            return get_common_response(available_models_classes)
+        except Exception as e:
+            return get_error_response(e)
+
+        
+@api.route('/api/models')
+class models_store(Resource):
+    @staticmethod
+    def get():
+        try:
+            return get_common_response(list(models_storege.keys()))
+
+        except Exception as e:
+            return get_error_response(e)
+
+
+fit_parser = reqparse.RequestParser()
+fit_parser.add_argument("model_class",
+                          type=str,
+                          required=True,
+                          help="Name of a class model you want to fit",
+                          location="args")
+fit_parser.add_argument("model_name",
+                          type=str,
+                          required=True,
+                          help="Custom name of a model you want to fit",
+                          location="args")
+fit_parser.add_argument('params',
+                          required=False,
+                          help='Model params or None',
+                          location="args")
+fit_parser.add_argument('data_with_target_path',
+                          type=str,
+                          required=True,
+                          help='Data path',
+                          location="args") #'swagger_api/data_sample/data_regression.csv' || 'swagger_api/data_sample/data_classification.csv'
+
+# fit_fields = api.model('Fit body', {
+#     'model_class': fields.String(enum=['Regression', 'Classification'], description='Name of a class model'),
+#     'model_name': fields.String(default = 'Regression_num_1', description='Custom name of a model'),
+#     'params': fields.Raw(description='Model params', required=False),
+#     'data_with_target_path' : fields.String(default = 'swagger_api/data_sample/data_regression.csv', description='Data path')
+# })
+
+
+@api.route('/api/models/fit')
+class models_fit(Resource):
+    @api.expect(fit_parser)
+#     @api.doc(body=fit_fields)
+
+    def post(self):
+        try:
+            args = fit_parser.parse_args()
+            if args.params is None:
+                params = None
+            else:
+                params = json.loads(args.params.replace("'", "\""))
+                
+            path = args.data_with_target_path
+            df = pd.read_csv(path, index_col=0)
+            train_data = df.drop(columns=['target'])
+            train_target = df['target']           
+            np.save('train_data.npy', train_data)
+            model_name = fit_model(args.model_class, args.model_name, params, train_data, train_target)
+            return get_common_response(model_name)
+        except Exception as e:
+            return get_error_response(e)
+
+
+predict_parser = reqparse.RequestParser()
+predict_parser.add_argument("model_name",
+                          type=str,
+                          required=True,
+                          help="Custom name of a model you want to predict",
+                          location="args")
+# predict_fields = api.model('Predict body', {
+#     'model_name': fields.String(description='Custom name of a model')
+# })
+
+@api.route('/api/models/predict')
+class predict(Resource):
+    @api.expect(predict_parser)
+#     @api.doc(body=predict_fields)
+
+    def post(self):
+        try:
+            args = predict_parser.parse_args(strict=True)
+            train_data = np.load('train_data.npy')
+            target_pred = get_preds(args.model_name, train_data)
+            return get_common_response(target_pred)
+        except Exception as e:
+            return get_error_response(e)
+
+
+params_parser = reqparse.RequestParser()
+params_parser.add_argument("model_name",
+                          type=str,
+                          required=True,
+                          help="Custom name of a model you want to get params",
+                          location="args")
+# params_fields = api.model('Params body', {
+#     'model_name': fields.String(description='Custom name of a model')
+# })
+
+@api.route('/api/models/get_params')
+class params(Resource):
+#     @api.doc(body=params_fields)
+    @api.expect(params_parser)
+    def post(self):
+        try:
+            args = params_parser.parse_args(strict=True)
+            params_mod = get_params(args.model_name)
+            return get_common_response(params_mod)
+        except Exception as e:
+            return get_error_response(e)
+
+
+delete_parser = reqparse.RequestParser()
+delete_parser.add_argument('model_name',
+                          type=str,
+                          required=True,
+                          help="Custom name of a model you want to get params",
+                          location="args")
+
+# delete_fields = api.model('Delete body', {
+#     'model_name': fields.String(description='Custom name of a model')
+# })
+
+@api.route('/api/models/delete')
+class delete(Resource):
+#     @api.doc(body=delete_fields)
+    @api.expect(delete_parser)
+    def delete(self):
+        try:
+            args = delete_parser.parse_args(strict=True)
+            delete_model(args.model_name)
+            return get_common_response([])
+        except Exception as e:
+            return get_error_response(e)
+        
+        
+class HttpStatus:
+    OK = 200
+    CREATED = 201
+    NOT_FOUND = 404
+    BAD_REQUEST = 400        
+     
+        
+def get_error_response(exception):
+    construct = {
+        'error': exception.__str__(),
+        'success': False,
+        'result': []
+    }
+    response = jsonify(construct)
+    response.status_code = HttpStatus.OK
+    return response
+
+
+def get_common_response(result):
+    construct = {
+        'error': [],
+        'success': True,
+        'result': result
+    }
+    response = jsonify(construct)
+    response.status_code = HttpStatus.OK
+    return response
+
+
+def fix_list(arr):
+    return [eval(item) for item in arr]
+
